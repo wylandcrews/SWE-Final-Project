0e3dff578aae192c19dbbf408da46b40f47dd5a9
JoseMose <88098496+JoseMose@users.noreply.github.com>
2021-11-12 21:59:04 -0500

    Add files via upload

---
+++ glogin.py
@@ -0,0 +1,104 @@
+import os
+import pathlib
+
+import requests
+from flask import Flask, session, abort, redirect, request, render_template
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+from dotenv import load_dotenv
+import google.auth.transport.requests
+
+app = Flask("Google Login App")
+app.secret_key = "CodeSpecialist.com"
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
+
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)  # Authorization required
+        else:
+            return function()
+
+    return wrapper
+
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/protected_area")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+
+@app.route("/")
+def index2():
+    return "Hello World <a href='/login'><button>Login</button></a>"
+
+
+@app.route("/protected_area")
+@login_is_required
+def protected_area():
+    return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a> <a href='index'><button>Welcome</button></a>"
+
+@app.route("/index")
+def index():
+    currentWeather = session.get("currentWeather", None)
+    weatherImage = session.get("weatherImage", None)
+    lat_recommendation = session.get("lat_recommendation", None)
+    lng_recommendation = session.get("lng_recommendation", None)
+
+    if currentWeather != None and weatherImage != None and lat_recommendation != None and lng_recommendation != None:
+        return render_template(
+            "index.html",
+            currentWeather=currentWeather,
+            weatherImage=weatherImage,
+            lat_recommendation=lat_recommendation,
+            lng_recommendation=lng_recommendation
+        )
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
